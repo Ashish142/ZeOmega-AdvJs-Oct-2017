d0d2ae36c96593264f764d25c0630d4af8e3fecd
Magesh <tkmagesh77@gmail.com>
2017-10-03 17:05:13 +0530

    Click tracker

--- 01-language/src/calculator.js
+++ 01-language/src/calculator.js
@@ -1,3 +1,14 @@
-function add(x,y){
-	return x + y;
+function add(){
+	function parseArg(n){
+		if (Array.isArray(n)) return add.apply(this, n)
+		if (typeof n === 'function') return parseArg(n());
+		return isNaN(n) ? 0 : parseInt(n,10);
+	}
+	return arguments.length <= 1 ? parseArg(arguments[0]) : parseArg(arguments[0]) + add([].slice.call(arguments, 1));
 }
+
+//When a function is invoked as a method of obj
+// this -> obj
+
+//When a function is invoked as a function
+// this -> global scope (window)
