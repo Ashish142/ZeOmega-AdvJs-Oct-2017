01d5eea34b16676d49815178556fa8d419a80a47
Magesh <tkmagesh77@gmail.com>
2017-10-04 10:55:32 +0530

    Closures - cont...

---
+++ 02-closures/spinner.js
@@ -0,0 +1,42 @@
+
+function spinnerFactory(){
+	var counter = 0;
+
+
+	function increment(){
+		return ++counter;
+	}
+
+	function decrement(){
+		return --counter;
+	}
+
+	return {
+		up : increment,
+		down : decrement
+	};
+}
+
+var spinner = spinnerFactory();
+
+//=================================================
+
+var spinner = (function (){
+	var counter = 0;
+
+
+	function increment(){
+		return ++counter;
+	}
+
+	function decrement(){
+		return --counter;
+	}
+
+	return {
+		up : increment,
+		down : decrement
+	};
+})();
+
+
