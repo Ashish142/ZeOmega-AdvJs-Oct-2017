7e509eda458ae71819ed5bbc313e60ca3c885de6
Magesh <tkmagesh77@gmail.com>
2017-10-04 11:23:05 +0530

    memoize - final

---
+++ 02-closures/memoize-2.js
@@ -0,0 +1,16 @@
+function memoize(algoFn){
+	var cache = {};
+
+	return function(){
+		var key = JSON.stringify(arguments);
+		if (typeof cache[key] === 'undefined')
+			cache[key] = algoFn.apply(undefined, arguments);
+		return cache[key];
+	}
+	
+}
+
+var add = memoize(function(x,y){
+	console.log('processing ', x , ' and ', y);
+	return x + y;
+});
