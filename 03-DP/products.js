79bd0526f02593c781089d903dd899b0f9b0c1c9
Magesh <tkmagesh77@gmail.com>
2017-10-04 12:40:08 +0530

    Sort - Complete

--- 03-DP/products.js
+++ 03-DP/products.js
@@ -34,17 +34,70 @@ describe('Sort', function(){
 		console.table(products);
 	});
 
-	/*describe('Any list by any attribute', function(){
+	function sort(list, comparer){
+		var comparerFn = undefined;
+		if (typeof comparer === 'function')
+			comparerFn = comparer;
+		if (typeof comparer === 'string'){
+			comparerFn = function(item1, item2){
+				if (item1[comparer] < item2[comparer]) return -1;
+				if (item1[comparer] > item2[comparer]) return 1;
+				return 0;
+			}
+		}
+		if (!comparerFn) return;
+		for(var i=0; i < list.length-1; i++)
+			for(var j=i+1; j < list.length; j++)
+				if (comparerFn(list[i], list[j]) > 0){
+					var temp = list[i];
+					list[i] = list[j];
+					list[j] = temp;
+				}
+	}
+	describe('Any list by any attribute', function(){
+		/*function sort(list, attrName){
+			for(var i=0; i < list.length-1; i++)
+				for(var j=i+1; j < list.length; j++)
+					if (list[i][attrName] > list[j][attrName]){
+						var temp = list[i];
+						list[i] = list[j];
+						list[j] = temp;
+					}
+		}*/
 		describe('Products by cost', function(){
-			//sort(products, .....);
+			sort(products, 'cost');
 			console.table(products);
 		});
 
 		describe('Products by units', function(){
-			//sort(products, .....);
+			sort(products, 'units');
 			console.table(products);
 		});
-	});*/
+	});
+
+	describe('Any list by any comparer', function(){
+		/*function sort(list, comparer){
+			for(var i=0; i < list.length-1; i++)
+				for(var j=i+1; j < list.length; j++)
+					if (comparer(list[i], list[j]) > 0){
+						var temp = list[i];
+						list[i] = list[j];
+						list[j] = temp;
+					}
+		}*/
+		describe('Products by value [value=cost * units]', function(){
+			var productComparerByValue = function(p1, p2){
+				var p1Value = p1.cost * p1.units,
+					p2Value = p2.cost * p2.units;
+				if (p1Value < p2Value) return -1;
+				if (p1Value > p2Value) return 1;
+				return 0;
+			}
+			sort(products, productComparerByValue);
+			console.table(products);
+		});
+
+	});
 });
 
 /*describe('Filter', function(){
