e93e6c58fc1abbff6e5102eb2cec1290d4a03ec0
Magesh <tkmagesh77@gmail.com>
2017-10-04 10:57:05 +0530

    isPrime

--- 02-closures/isPrime.js
+++ 02-closures/isPrime.js
@@ -1,3 +1,41 @@
+//cache is private
+var isPrime = (function(){
+	var cache = {};
+
+	function isPrime(n){
+		if (typeof cache[n] !== 'undefined')
+			return cache[n];
+
+		console.log('processing ', n);
+		cache[n] = true;
+		for(var index = 2, count = (n/2); index < count; index++){
+			if (n % index === 0){
+				cache[n] = false;
+				break;
+			}
+		}
+		return cache[n];
+	}
+	return isPrime;
+})();
+
+//cache can be public
+
+
 function isPrime(n){
-	
+	if (!isPrime.cache){
+		isPrime.cache = {};
+	}
+	if (typeof isPrime.cache[n] !== 'undefined')
+		return isPrime.cache[n];
+
+	console.log('processing ', n);
+	isPrime.cache[n] = true;
+	for(var index = 2, count = (n/2); index < count; index++){
+		if (n % index === 0){
+			isPrime.cache[n] = false;
+			break;
+		}
+	}
+	return isPrime.cache[n];
 }
